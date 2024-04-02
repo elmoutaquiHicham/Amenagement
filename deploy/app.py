82586499c5ad4c32e782cf3ed523fefa96d77b9e
Hicham <101475744+elmoutaquiHicham@users.noreply.github.com>
2024-04-02 23:27:00 +0200

    Add files via upload

---
+++ deploy/app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+
+# Chargement des données depuis un fichier CSV (ou une autre source de données)
+@st.cache
+def load_data():
+    # Remplacez 'nom_du_fichier.csv' par le nom de votre fichier CSV contenant les données des bungalows
+    return pd.read_csv('C:\\Users\\H.ELMOUTAQUI\\OneDrive - BYCN\\Bureau\\Deploy\\bungalow_tk.xlsx')
+
+# Fonction pour filtrer les références en fonction des valeurs sélectionnées
+def filter_references(data, filters):
+    filtered_data = data.copy()
+    for column, value in filters.items():
+        if value is not None:
+            filtered_data = filtered_data[filtered_data[column] == value]
+    return filtered_data['reference']
+
+# Titre de l'application
+st.title('Sélection de Bungalows')
+
+# Chargement des données
+data = load_data()
+
+# Création des sélecteurs pour chaque colonne
+filters = {}
+for column in data.columns[2:]:  # Commence à partir de la troisième colonne pour exclure 'reference' et 'sous_famille'
+    selected_value = st.selectbox(f'Sélectionnez une valeur pour {column}', [None] + data[column].unique())
+    filters[column] = selected_value
+
+# Bouton pour filtrer les références
+if st.button('Filtrer'):
+    filtered_references = filter_references(data, filters)
+    if filtered_references.empty:
+        st.write('Aucune référence ne correspond aux critères sélectionnés.')
+    else:
+        st.write('Références correspondantes :')
+        st.write(filtered_references)
